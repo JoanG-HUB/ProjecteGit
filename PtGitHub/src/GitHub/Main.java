3b7726039108bf5bb9428a70fbf84ae549c691d7
dam1 <dam1@A80533400647799>
2025-02-13 10:42:15 +0100

    Second commit

--- PtGitHub/src/GitHub/Main.java
+++ PtGitHub/src/GitHub/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		  System.out.println("Hola, món!");
 		  System.out.println("1a modificació Git");
+		  System.out.println("2a modificació Git");
+
 	}
 
 }
